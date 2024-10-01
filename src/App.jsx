5227143c20588e6124288237482693bbbf65c778
efunzz <109739577+efunzz@users.noreply.github.com>
2024-10-01 22:13:38 +0800

    create-form-component-v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./index.css";
 import Hero from "./components/HeroSection";
+import Form from "./components/FormSection";
 function App() {
   return (
     <>
       <Hero />
+      <Form />
     </>
   );
 }
