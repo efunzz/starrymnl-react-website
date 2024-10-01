9eb1b8d24333e5e5dd0bccb51f66fb7f20b812c2
efunzz <109739577+efunzz@users.noreply.github.com>
2024-10-01 22:25:31 +0800

    removing-complicated-libraries

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,10 +3,9 @@ import { Dialog, DialogPanel } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const navigation = [
-  { name: "Product", href: "#" },
-  { name: "Features", href: "#" },
-  { name: "Marketplace", href: "#" },
-  { name: "Company", href: "#" },
+  { name: "About", href: "#" },
+  { name: "Illustrations", href: "#" },
+  { name: "Products", href: "#" },
 ];
 
 export default function Hero() {
@@ -22,11 +21,6 @@ export default function Hero() {
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img
-                alt=""
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                className="h-8 w-auto"
-              />
             </a>
           </div>
           <div className="flex lg:hidden">
